ea88f04ac6142de363f3cc5fa1febb9cbae84629
Daniel Benedict <daniel2501@gmail.com>
2023-12-24 15:45:51 -0500

    No need for the while loop. Need to suspend entr. Real fix.

--- push-org-files.sh
+++ push-org-files.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-while sleep 1; do find /home/daniel/org | entr -s 'bash /home/daniel/shell-scripts/add-commit-push.sh'; done
+find /home/daniel/org | entr sh $HOME/shell-scripts/add-commit-push.sh &
