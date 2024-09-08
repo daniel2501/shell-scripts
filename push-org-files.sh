09ad8d5cb506befb88ab1f75fb78ddeafa2b622d
Daniel Benedict <daniel2501@gmail.com>
2024-09-08 18:22:51 -0400

    merge conflict

--- push-org-files.sh
+++ push-org-files.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env sh
 
-<<<<<<< HEAD
 pgrep -f add-commit-push.sh && exit
-find /home/daniel/org | entr sh $HOME/shell-scripts/add-commit-push.sh &
-=======
 find $HOME/org | entr sh $HOME/shell-scripts/add-commit-push.sh &
->>>>>>> origin/trunk
