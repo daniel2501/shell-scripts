6cfcb860870eb24438bfebb2d55bca016b2be8dc
Daniel Benedict <daniel2501@gmail.com>
2024-11-27 09:30:15 -0500

    guix run-doom.sh to inject ssh_auth_sock

--- run-doom.sh
+++ run-doom.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # Fish shell needs -c instead of -s
-eval "$(ssh-agent -c)"
+eval "$(ssh-agent -s)"
 # Assumes existence of github.pem
 ssh-add ~/.ssh/github.pem
-echo $SSH_AUTH_SOCK | wl-copy
+echo $SSH_AUTH_SOCK
+SSH_AUTH_SOCK=$SSH_AUTH_SOCK $HOME/.config/emacs/bin/doom run
